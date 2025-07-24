7243aac7597975690810b69e3028abbf1d8a9e39
PULUKURIHARITHA <divyalakshmanrao1@gmail.com>
2025-07-24 10:06:48 +0530

    Update tree recursions.py

--- tree recursions.py
+++ tree recursions.py
@@ -7,3 +7,7 @@ terms=int(input("enter number of terms to print:"))
 print(("fibonacci series........"))
 for i in range(terms):
     print(fib(i),end=' ')
+
+OUTPUT:-
+enter number of terms to print:20
+0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181
